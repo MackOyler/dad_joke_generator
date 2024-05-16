80ef5a8f61c27d784bef8486ad0a0fd32170d859
Mack Oyler <120589371+MackOyler@users.noreply.github.com>
2024-05-16 14:48:10 -0500

    dad joke generator complete

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-const btnEL = document.getElementById("btn")
+const btnEL = document.getElementById("btn");
+const jokeEL = document.getElementById("joke");
 
 const apiKey = "tnyduVrv7KpMS0bMfG+omg==cN5W9gPxay9fdBgf";
 
@@ -12,10 +13,23 @@ const options = {
 const apiURL = "https://api.api-ninjas.com/v1/dadjokes"
 
 async function getJoke() {
+  try {
+    jokeEL.innerText = "Loading...";
+    btnEL.disable = true;
+    btnEL.innerText = "maximizing cringe...";
     const response = await fetch(apiURL, options);
     const data = await response.json();
 
-    console.log(data[0].joke);
+    btnEL.disable = false;
+    btnEL.innerText = "TELL ME A JOKE!";
+
+    jokeEL.innerText = data[0].joke;
+  } catch (error) {
+    jokeEL.innerText = "An error happened, try again.";
+    btnEL.disable = false;
+    btnEL.innerText = "TELL ME A JOKE!";
+    console.log(error);
+  }       
 }
 
 btnEL.addEventListener("click", getJoke);
